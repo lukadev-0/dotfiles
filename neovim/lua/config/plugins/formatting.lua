c81f2040dec81b61d9d181660f0af6436ef13ba0
LukaDev <hi@lukadev.me>
2025-03-05 16:47:27 +0100

    feat: add more filetypes for formatting

--- neovim/lua/config/plugins/formatting.lua
+++ neovim/lua/config/plugins/formatting.lua
@@ -5,6 +5,9 @@ return {
             formatters_by_ft = {
                 lua = { "stylua" },
                 javascript = { "prettierd" },
+                typescript = { "prettierd" },
+                typescriptreact = { "prettierd" },
+                javascriptreact = { "prettierd" },
             },
             default_format_opts = {
                 lsp_format = "fallback",
