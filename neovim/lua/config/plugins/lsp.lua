aad1422023bea4ce8d922147b7143ea61ad85ed5
LukaDev <hi@lukadev.me>
2025-03-07 14:24:10 +0100

    feat: add fidget for LSP progress

--- neovim/lua/config/plugins/lsp.lua
+++ neovim/lua/config/plugins/lsp.lua
@@ -79,6 +79,21 @@ return {
             },
         },
     },
+    {
+        "j-hui/fidget.nvim",
+        opts = {
+            progress = {
+                display = {
+                    done_icon = "󰄬",
+                },
+            },
+            notification = {
+                window = {
+                    winblend = 0,
+                },
+            },
+        },
+    },
 
     -- Lua
     {
