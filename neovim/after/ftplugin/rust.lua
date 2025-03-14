c512658d8c6fec45f44110bd43672db16d8cec20
LukaDev <hi@lukadev.me>
2025-03-14 20:15:22 +0100

    fix: rust language stuff

--- neovim/after/ftplugin/rust.lua
+++ neovim/after/ftplugin/rust.lua
@@ -3,9 +3,20 @@ vim.keymap.set("i", "'", "'", { buffer = true })
 
 -- Rustaceanvim
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set("n", "<leader>a", function()
+
+local function map(keys, fn, desc)
+    vim.keymap.set("n", keys, fn, { buffer = bufnr, desc = "LSP: " .. desc })
+end
+
+map("<leader>ca>", function()
     vim.cmd.RustLsp("codeAction")
-end, { silent = true, buffer = bufnr })
-vim.keymap.set("n", "K", function()
+end, "Code Action")
+
+map("K", function()
     vim.cmd.RustLsp({ "hover", "actions" })
-end, { silent = true, buffer = bufnr })
+end, "Code Action")
+
+map("<leader>cr", vim.lsp.buf.rename, "Rename")
+map("<leader>ca", vim.lsp.buf.code_action, "Code Action")
+map("gd", Snacks.picker.lsp_definitions, "Goto Definition")
+map("gr", Snacks.picker.lsp_references, "Goto References")
