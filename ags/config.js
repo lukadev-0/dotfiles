f05803920158acee2b7107dda0c54c319c7abe1f
LukaDev <hi@lukadev.me>
2025-06-08 11:44:36 +0200

    nuke everything

--- ags/config.js
+++
@@ -1,3 +0,0 @@
-await Utils.execAsync(["bash", "-c", `cd ${App.configDir} && bun run build`]);
-
-await import("./out/main.js");
--- ags/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { Bar } from "./widgets/bar";
-import { Background } from "./widgets/background";
-
-App.config({
-  windows: [Bar(0), Background(0)],
-  style: "./out/main.css",
-});
