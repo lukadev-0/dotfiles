635b235befc6ec336a7e2685926fe41a01913ba0
LukaDev <hi@lukadev.me>
2025-07-11 16:39:59 +0200

    fix tmux shenanigans

--- home/tmux.nix
+++ home/tmux.nix
@@ -9,13 +9,19 @@
     clock24 = true;
     escapeTime = 0;
     sensibleOnTop = true;
+    terminal = "tmux-256color";
     plugins = with pkgs; [
       tmuxPlugins.catppuccin
       tmuxPlugins.yank
     ];
     extraConfig = ''
-      set-option -sa terminal-overrides ",xterm*:Tc"
+      set-option -a terminal-features ',xterm*:RGB'
       set-option -g renumber-windows on
+
+      if-shell 'test -n "$WSL_DISTRO_NAME"' {
+        set -as terminal-overrides ',*:Setulc=\E[58::2::::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+        set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
+      }
     '';
   };
 }
