01073d3bb5c3195dfa43c25a9a9280c70d6f2e0f
LukaDev <hi@lukadev.me>
2025-08-28 14:40:05 +0200

    increase padding

--- home/with/ui/ghostty.nix
+++ home/with/ui/ghostty.nix
@@ -7,8 +7,8 @@
       theme = "catppuccin-mocha";
       font-family = "GeistMono NF";
       font-size = 11;
-      window-padding-x = 5;
-      window-padding-y = 5;
+      window-padding-x = 8;
+      window-padding-y = 8;
     };
   };
 }
