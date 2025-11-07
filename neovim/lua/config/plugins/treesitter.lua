90f570032729df86e9897e555ec06f7e92f15c3f
LukaDev <hi@lukadev.me>
2025-11-07 19:20:26 +0100

    neovim

--- neovim/lua/config/plugins/treesitter.lua
+++ neovim/lua/config/plugins/treesitter.lua
@@ -6,9 +6,7 @@ return {
         config = function()
             vim.api.nvim_create_autocmd("FileType", {
                 callback = function()
-                    if pcall(vim.treesitter.start) then
-                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-                    end
+                    pcall(vim.treesitter.start)
                 end,
             })
         end,
