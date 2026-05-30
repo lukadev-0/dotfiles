14aeddc2ff21601f4c40eb3e3e3d37a216af6bc3
LukaDev <hi@lukadev.me>
2026-05-30 23:13:02 +0200

    nuke neovim

--- neovim/lsp/astro.lua
+++
@@ -1,7 +0,0 @@
-return {
-    init_options = {
-        typescript = {
-            tsdk = vim.fn.stdpath("data") .. "/luka/typescript/lib",
-        },
-    },
-}
--- neovim/lsp/rust-analyzer.lua
+++
@@ -1,9 +0,0 @@
-return {
-    settings = {
-        ["rust-analyzer"] = {
-            cargo = {
-                features = "all",
-            },
-        },
-    },
-}
