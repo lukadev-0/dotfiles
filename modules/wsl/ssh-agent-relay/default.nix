10abcceac1408ea18aa9243fdc8f1dee247fd5ea
LukaDev <hi@lukadev.me>
2026-08-03 02:31:13 +0200

    drop socat in wsl ssh agent relay

--- modules/wsl/ssh-agent-relay/default.nix
+++ modules/wsl/ssh-agent-relay/default.nix
@@ -10,18 +10,24 @@ in
 
     systemd.user.sockets.wsl-ssh-agent-relay = {
       Unit.Description = "Socket for WSL SSH Agent Relay";
-      Socket.ListenStream = "%t/${socketName}";
+      Socket = {
+        ListenStream = "%t/${socketName}";
+        Accept = true;
+      };
       Install.WantedBy = [ "sockets.target" ];
     };
 
-    systemd.user.services.wsl-ssh-agent-relay = {
+    systemd.user.services."wsl-ssh-agent-relay@" = {
       Unit = {
         Description = "WSL SSH Agent Relay";
         Requires = [ "wsl-ssh-agent-relay.socket" ];
       };
       Service = {
         Type = "exec";
-        ExecStart = "${getExe pkgs.socat} ACCEPT-FD:3,fork EXEC:\"${getExe npiperelay} -ei -s //./pipe/openssh-ssh-agent\",nofork";
+        StandardInput = "socket";
+        StandardOutput = "socket";
+        StandardError = "journal";
+        ExecStart = "${getExe npiperelay} -ei -s //./pipe/openssh-ssh-agent";
       };
     };
   };
